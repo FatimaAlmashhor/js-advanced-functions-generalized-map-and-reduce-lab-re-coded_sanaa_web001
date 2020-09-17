e3af9c52078551410d85868f68f1409a53492279
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-17 03:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ arr = map(dune , callbacks)
 
 
 
-function reduce ( arr , callbackFun , init = 0 ){
+function reduce ( arr , callbackFun , init  ){
   if(init = 0)
     return  arr.reduce(callbackFun ) ;
   else {
